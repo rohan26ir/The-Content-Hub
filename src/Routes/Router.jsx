bea1d5d98bedb08cd16a2dad36858016eefd5614
Rohan <rohan26ir@gmail.com>
2024-12-23 20:41:55 +0600

    add Main Layout

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,11 +2,22 @@ import {
   createBrowserRouter,
   // RouterProvider,
 } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout/MainLayout";
+import Error from "../components/Error/Error";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    errorElement: <Error></Error>,
+    children: [
+      
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+import RightLayout from '../RightLayout';
 
 const MainLayout = () => {
   return (
     <div>
+
+      <header>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      </header>
+
+      {/* Main */}
+      <main>
+        <div>
+          <div>
+          <Outlet></Outlet>
+          </div>
+          <div>
+            <RightLayout></RightLayout>
+          </div>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer>
+        <Footer></Footer>
+      </footer>
       
     </div>
   );
