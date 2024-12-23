a2a63e8be7473fbf4d8d4c269afd9136425ee413
Rohan <rohan26ir@gmail.com>
2024-12-23 20:13:54 +0600

    first commit

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,13 @@
+import {
+  createBrowserRouter,
+  // RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default router;
